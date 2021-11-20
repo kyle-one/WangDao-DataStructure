b81736c63618994e1d71bcbd9889e3fc785e160d
Kyle <44631434+kyle-one@users.noreply.github.com>
2021-11-20 15:39:29 +0800

    Update floyd.cpp

--- 图/floyd.cpp
+++ 图/floyd.cpp
@@ -1,19 +1,20 @@
 void floyd() {
-        // k为中间节点的下标
-        for (int k = 0; k < dis.length; k++) {
-            // i为开始节点的下标
-            for (int i = 0; i < dis.length; i++) {
-                // j为结束节点的下标
-                for (int j = 0; j < dis.length; j++) {
-                    // 计算以k为中间节点，i为开始节点，j为结束节点，i->k->j的距离
-                    int length = dis[i][k] + dis[k][j];
-                    // 将i->k->j的距离与i->j的距离进行比较
-                    // 如果i->j的距离大，则更新距离表和前驱节点表
-                    if (length < dis[i][j]) {
-                        dis[i][j] = length;
-                        pre[i][j] = pre[i][k];
-                    }
+    // k为中间节点的下标
+    for (int k = 0; k < dis.length; k++) {
+        // i为开始节点的下标
+        for (int i = 0; i < dis.length; i++) {
+            // j为结束节点的下标
+            for (int j = 0; j < dis.length; j++) {
+                // 计算以k为中间节点，i为开始节点，j为结束节点，i->k->j的距离
+                int length = dis[i][k] + dis[k][j];
+                // 将i->k->j的距离与i->j的距离进行比较
+                // 如果i->j的距离大，则更新距离表和前驱节点表
+                if (length < dis[i][j]) {
+                    dis[i][j] = length;
+                    pre[i][j] = pre[i][k];
                 }
             }
         }
+    }
 }
+
